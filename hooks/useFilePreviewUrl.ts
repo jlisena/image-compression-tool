eb04d9fe54d4119595454d54fdd4fb7b974919e1
Jon <jlisena@asu.edu>
2025-12-23 23:36:40 -0700

    Update useFilePreviewUrl comment

--- hooks/useFilePreviewUrl.ts
+++ hooks/useFilePreviewUrl.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 /**
- * Creates a preview URL for a given File object and cleans it up automatically.
+ * Creates a preview URL for a given File object and cleans it up automatically. It's used in image-card.tsx to display the thumbnail preview of each compressed image.
  */
 
 export function useFilePreviewUrl(file: File | null): string | null {
